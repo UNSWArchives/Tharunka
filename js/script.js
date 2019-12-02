ecb20ab9ef98d6a4085f63682fe8439829792eef
UNSWArchives <katie.bird@unsw.edu.au>
2019-12-03 09:36:31 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ $(function(){
     function clean_query() {
         var query;
         if (useHttps === 'false') {
-            query = 'https://api.trove.nla.gov.au/v2/result?zone=newspaper&l-category=Article&encoding=json'
+            query = 'https://api.trove.nla.gov.au/v2/result?zone=newspaper&category=Article&encoding=json'
         } else {
             query = 'https://trove-proxy.herokuapp.com/api/result?zone=newspaper&l-category=Article&encoding=json'
         }
